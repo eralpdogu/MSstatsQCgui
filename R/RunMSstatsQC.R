f0e73cd95100668c196ea0557474b64b2fe54afd
Eralp DOGU <eralp.dogu@gmail.com>
2018-03-19 16:34:28 +0200

    last changes before submission

--- R/RunMSstatsQC.R
+++ R/RunMSstatsQC.R
@@ -4,10 +4,13 @@
 #'  This plot provides massages about overall system performance.
 #' @keywords GUI shiny
 #' @export
-#' @import shiny
+#' @import shiny MSstatsQC
 #' @return A connection for the shiny interface
 #' @examples
-#' \dontrun{ RunMSstatsQC() }
+#' \dontrun{An example dataset can be found through MSstatsQC package}
+#' library(MSstatsQC)
+#' data <- MSstatsQC::S9Site54
+#' \dontrun{RunMSstatsQC()}
 
 RunMSstatsQC <- function() {
   appDir <- system.file("shiny-examples", "MSstatsQCgui", package = "MSstatsQCgui")
